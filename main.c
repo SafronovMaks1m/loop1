3b17576965a89954102735c87fe235d49b4c86c5
SafronovMaks1m <safranov01@list.ru>
2023-09-29 14:35:18 +0300

    task

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define min(x,y) (((x)>(y)) ? (x) : (y))
+void main() {
+    int a, b, c;
+    do{
+    scanf("%d  %d", &a, &b);
+    for (int i = 1; i <= min(a, b)/2; i++){
+        if (a%i == 0 && b%i == 0){
+            c = i;}}
+} while(a>1000 || a<=1 || b>1000 || b<=1);
+printf("HOD = %d\n", c);
+}
