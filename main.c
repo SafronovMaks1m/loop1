b67b9dad68bbcb036a2bfe0b5bf778437d6c845a
SafronovMaks1m <144445164+SafronovMaks1m@users.noreply.github.com>
2023-10-20 15:50:36 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,12 +1,18 @@
-#include <stdio.h>
-#include <stdlib.h>
-#define min(x,y) (((x)>(y)) ? (x) : (y))
-void main() {
-    int a, b, c;
-        while(a>1000 || a<=1 || b>1000 || b<=1){
-        scanf("%d  %d", &a, &b);
-        for (int i = 1; i <= min(a, b)/2; i++){
-            if (a%i == 0 && b%i == 0){
-                c = i;}}} 
-printf("HOD = %d\n", c);
+#include <math.h>
+
+char find_bin_pattern(int number){
+    int mas[100] = {}, total = 0, count = 0;
+    while (number>0){
+        mas[total] = number%2;
+        number = number/2;
+        total+=1;
+    }
+    for (int i = 0; i<97; i++){
+        if (mas[i] == 1 && mas[i+1] == 0 && mas[i+2] == 1){
+            count+=1;
+            mas[i] = 0;
+            mas[i+1] = 0;
+            mas[i+2] = 0;}
+    }
+    return count;
 }
